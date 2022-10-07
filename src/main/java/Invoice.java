a706142c13721466f963b9ea5fd11bdc6b9eed7f
mathieu <mathieu.barton@etu.uca.fr>
2022-10-07 13:42:04 +0200

    Correction

--- src/main/java/Invoice.java
+++ src/main/java/Invoice.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class Invoice {
 
   public String customer;
-  public ArrayList<Performance> performances;
+  public List<Performance> performances;
 
-  public Invoice(String customer, ArrayList<Performance> performances) {
+  public Invoice(String customer, List<Performance> performances) {
     this.customer = customer;
     this.performances = performances;
   }
--- src/test/java/StatementPrinterTests.java
+++ src/test/java/StatementPrinterTests.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -10,10 +11,11 @@ public class StatementPrinterTests {
 
     @Test
     void exampleStatement() {
-        Map<String, Play> plays = Map.of(
-                "hamlet",  new Play("Hamlet", "tragedy"),
-                "as-like", new Play("As You Like It", "comedy"),
-                "othello", new Play("Othello", "tragedy"));
+
+        HashMap<String, Play> plays = new HashMap<>();
+        plays.put("hamlet",  new Play("Hamlet", "tragedy"));
+        plays.put("hamlet",  new Play("As You Like It", "comedy"));
+        plays.put("hamlet",  new Play("Othello", "tragedy"));
 
         Invoice invoice = new Invoice("BigCo", List.of(
                 new Performance("hamlet", 55),
@@ -28,9 +30,10 @@ public class StatementPrinterTests {
 
     @Test
     void statementWithNewPlayTypes() {
-        Map<String, Play> plays = Map.of(
-                "henry-v",  new Play("Henry V", "history"),
-                "as-like", new Play("As You Like It", "pastoral"));
+
+        HashMap<String, Play> plays = new HashMap<>();
+        plays.put("henry-v",  new Play("Henry V", "history"));
+        plays.put("as-like",  new Play("As You Like It", "pastoral"));
 
         Invoice invoice = new Invoice("BigCo", List.of(
                 new Performance("henry-v", 53),
